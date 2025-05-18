b221539f136bae9a1020fa06f4cba8321416644c
Jardielson <jardielsonbio@gmail.com>
2025-05-18 14:07:57 -0300

    feat: added sqs and dontenv

--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './mongoose/models/users/user.model';
 import { UserRepository } from './mongoose/repositories/users/user.repository';
+import { SqsModule } from '@ssut/nestjs-sqs';
+import { SqsProduce } from './sqs/sqs.producer';
 
 @Module({
   imports: [
@@ -9,8 +11,18 @@ import { UserRepository } from './mongoose/repositories/users/user.repository';
       process.env.MONGO_URI || 'mongodb://localhost:27018/vehicles-db',
     ),
     MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    SqsModule.register({
+      consumers: [],
+      producers: [
+        {
+          name: String(process.env.AWS_SQS_QUEUE_NAME),
+          queueUrl: String(process.env.AWS_SQS_QUEUE_URL),
+          region: String(process.env.AWS_REGION),
+        },
+      ],
+    }),
   ],
-  providers: [UserRepository],
-  exports: [UserRepository],
+  providers: [UserRepository, SqsProduce],
+  exports: [UserRepository, SqsProduce],
 })
 export class InfraModule {}
---
+++ src/infra/sqs/sqs.producer.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { SqsService } from '@ssut/nestjs-sqs';
+import { v4 } from 'uuid';
+
+@Injectable()
+export class SqsProduce {
+  constructor(private readonly sqsService: SqsService) {}
+
+  async sendToQueue(entity: string, input: any) {
+    try {
+      await this.sqsService.send(String(process.env.AWS_SQS_QUEUE_NAME), {
+        id: v4(),
+        body: {
+          entity,
+          data: JSON.stringify(input),
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
